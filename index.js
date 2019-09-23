404f4c0d9522bbce4d53481d09d1ad954702a042
Yura Prus <40232527+prusyura@users.noreply.github.com>
2019-09-23 19:26:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
 var app = express();
 //bodyParser
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: false}));
 //Server FrontPage
-app.get("/", function(req, res){
-    req.send('testBot server run');
-});
+app.get('/', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(messengerButton);
+    res.end();
+  });
 //Messenger
-app.get("/webhook", function(req, res){
-    if(req.query['hub.mode'] === 'subscribe' && 
-        req.query['hub.verify_token'] --- 'testbot_verify_token'){
-        console.log("Validating webhook");
-        res.send(req.query['hub.challenge']);
-    }else{
-        console.error("Failed validation. Make sure the validation tokens match.");
-        res.send('Invalid verify token');
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);          
     }
-});
-app.listen(process.env.PORT || 8080);
+  });
+app.listen(process.env.PORT || 3000);
 console.log("run server!");
